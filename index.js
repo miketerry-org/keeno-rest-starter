5a6efa15b4ac88b35fce2c5de8d3724c4c8da374
Mike Terry <support@miketerry.org>
2025-05-23 10:23:12 -0500

    added encrypted env files

---
+++ index.js
@@ -0,0 +1,9 @@
+// index.js: main entry point for keeno-rest-starter
+
+"use strict";
+
+// load all necessary modules
+const config = require("keeno-config");
+
+console.log("config", config);
+console.log("hello");
