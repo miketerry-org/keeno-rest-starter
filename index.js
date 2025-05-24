cf2d670afe7eff03b1ae2f4c1e47f9f0355899e5
Mike Terry <support@miketerry.org>
2025-05-24 04:24:20 -0500

    made changes to .env files

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 "use strict";
 
 // load all necessary modules
-const config = require("keeno-config");
+const { server, tenantManager } = require("keeno-config");
 
-console.log("config", config);
+// console.log("server", server);
+console.log();
+// console.log("tenantManager", tenantManager);
 console.log("hello");
